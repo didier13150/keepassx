17e9af19c81d62996c2b9038c64326c386c441c1
Didier Fabert <dfabert@b2pweb.com>
2015-11-09 18:26:34 +0100

    Add header to script

--- create-win-setup.sh.cmake
+++ create-win-setup.sh.cmake
@@ -1,4 +1,36 @@
 #!/bin/bash
+#############################################################################
+#      ____              ____    _       _   _                              #
+#     /# /_\_           |  _ \  (_)   __| | (_)   ___   _ __                #
+#    |  |/o\o\          | | | | | |  / _` | | |  / _ \ | '__|               #
+#    |  \\_/_/          | |_| | | | | (_| | | | |  __/ | |                  #
+#   / |_   |            |____/  |_|  \__,_| |_|  \___| |_|                  #
+#  |  ||\_ ~|                                                               #
+#  |  ||| \/                                                                #
+#  |  |||                                                                   #
+#  \//  |                                                                   #
+#   ||  |       Developper : Didier FABERT <didier.fabert@gmail.com>        #
+#   ||_  \      Date : 2015, November                                       #
+#   \_|  o|                                             ,__,                #
+#    \___/      Copyright (C) 2009 by didier fabert     (oo)____            #
+#     ||||__                                            (__)    )\          #
+#     (___)_)   File : create-win-setup.sh                 ||--|| *         #
+#                                                                           #
+#   This program is free software; you can redistribute it and/or modify    #
+#   it under the terms of the GNU General Public License as published by    #
+#   the Free Software Foundation; either version 3 of the License, or       #
+#   (at your option) any later version.                                     #
+#                                                                           #
+#   This program is distributed in the hope that it will be useful,         #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#   GNU General Public License for more details.                            #
+#                                                                           #
+#   You should have received a copy of the GNU General Public License       #
+#   along with this program; if not, write to the                           #
+#   Free Software Foundation, Inc.,                                         #
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.            #
+#############################################################################
 
 #ARCH can be i686 or x86_64
 ARCH=@WIN_BUILD_ARCH@
